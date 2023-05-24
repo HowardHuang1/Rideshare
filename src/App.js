08d141f0d079726e127d87dc036aa8901ee14308
inmessionante123 <dhruva.sankhe28@gmail.com>
2023-05-24 14:17:59 -0700

    Created Home Page
    Note: have created a temporary new page for the tesla image, as that isnt the first page users should land on.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
 import Home from './components/Home';
 import CreateRide from './components/CreateRide';
 import ProfileApp from './components/Profile';
@@ -14,7 +15,8 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/home" element={<Home/>} />
           <Route path="/createRide" element={<CreateRide/>} />
           <Route path="/profile" element={<ProfileApp/>} />
           <Route path="/login"   element={<Login/>} />
---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import RedirectingButton from './RedirectingButton';
+import "./HomePage.css";
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import '../App.css';
+
+function HomePage() {
+    return(
+        <>
+
+        </>
+    )
+    }
+
+    export default HomePage;  
