bcf10b2617eb3499f168168ebe71af0e4fa688d4
RohilKalra <68201571+RohilKalra@users.noreply.github.com>
2023-06-04 01:32:28 -0700

    fixed update button

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './Profile.css';
-import Modal from './Modal'
-import { processDate } from './PrettyDate';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Profile.css";
+import Modal from "./Modal";
+import { processDate } from "./PrettyDate";
 
-const username = localStorage.getItem('username'); 
+const username = localStorage.getItem("username");
 
-const Profile = ({username}) => {
+const Profile = ({ username }) => {
   console.log(username);
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/user-data', { params: { username } });
+        const response = await axios.get("http://localhost:8000/user-data", {
+          params: { username },
+        });
         setUserData(response.data);
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       }
     };
 
@@ -30,27 +32,39 @@ const Profile = ({username}) => {
         <table className="profile-table">
           <tbody>
             <tr>
-              <td><strong>Full Name:</strong></td>
+              <td>
+                <strong>Full Name:</strong>
+              </td>
               <td>{userData.fullName}</td>
             </tr>
             <tr>
-              <td><strong>Email:</strong></td>
+              <td>
+                <strong>Email:</strong>
+              </td>
               <td>{userData.emailAddress}</td>
             </tr>
             <tr>
-              <td><strong>Username:</strong></td>
+              <td>
+                <strong>Username:</strong>
+              </td>
               <td>{userData.username}</td>
             </tr>
             <tr>
-              <td><strong>Money Saved:</strong></td>
-              <td>{"$"+parseInt(userData.moneySaved)}</td>
+              <td>
+                <strong>Money Saved:</strong>
+              </td>
+              <td>{"$" + parseInt(userData.moneySaved)}</td>
             </tr>
             <tr>
-              <td><strong>Carbon Saved:</strong></td>
+              <td>
+                <strong>Carbon Saved:</strong>
+              </td>
               <td>{parseInt(userData.carbonSaved)} kg CO2</td>
             </tr>
             <tr>
-              <td><strong>Number of Rides:</strong></td>
+              <td>
+                <strong>Number of Rides:</strong>
+              </td>
               <td>{parseInt(userData.numRides)}</td>
             </tr>
           </tbody>
@@ -62,37 +76,44 @@ const Profile = ({username}) => {
   );
 };
 
-const RideHistory = ({username}) => {
+const RideHistory = ({ username }) => {
   const [rideData, setRideData] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
   const [selectedRideID, setSelectedRideID] = useState(null);
-  const [time, setTime] = useState(null);
+  // const [time, setTime] = useState(null);
   const [numRiders, setNumRiders] = useState(null);
 
   const leaveRide = async (username, rideID) => {
     try {
-      const response = await axios.post('http://localhost:8000/leave-ride', {username: username, rideID: rideID});
+      const response = await axios.post("http://localhost:8000/leave-ride", {
+        username: username,
+        rideID: rideID,
+      });
       if (response.status === 200) {
-        setRideData(prevRideData => prevRideData.filter(ride => ride._id !== rideID));
+        setRideData((prevRideData) =>
+          prevRideData.filter((ride) => ride._id !== rideID)
+        );
       }
     } catch (error) {
-      console.error('Error leaving ride:', error);
+      console.error("Error leaving ride:", error);
     }
   };
 
   useEffect(() => {
     const fetchRideData = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/get-rides-for-user', { params: { username } });
+        const response = await axios.get(
+          "http://localhost:8000/get-rides-for-user",
+          { params: { username } }
+        );
         setRideData(response.data);
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       }
     };
 
     fetchRideData();
   }, []);
-  
 
   const existingRides = [];
   const pastRides = [];
@@ -109,8 +130,8 @@ const RideHistory = ({username}) => {
   }
 
   const handleSubmit = () => {
-    <App/>
-  }
+    <App />;
+  };
 
   return (
     <div className="ride-history-container">
@@ -124,26 +145,51 @@ const RideHistory = ({username}) => {
                 {existingRides.map((ride) => (
                   <React.Fragment key={ride._id}>
                     <tr>
-                      <td><strong>Start:</strong></td>
+                      <td>
+                        <strong>Start:</strong>
+                      </td>
                       <td>{ride.locationFrom}</td>
                     </tr>
                     <tr>
-                      <td><strong>End:</strong></td>
+                      <td>
+                        <strong>End:</strong>
+                      </td>
                       <td>{ride.locationTo}</td>
                     </tr>
                     <tr>
-                      <td><strong>Date:</strong></td>
-                      <td>{processDate(time)}</td>
+                      <td>
+                        <strong>Date:</strong>
+                      </td>
+                      <td>{processDate(ride.date)}</td>
                     </tr>
                     <tr>
                       <td colSpan="2">
                         <div className="button-group">
-                          <button onClick={() => {setIsOpen(true); setSelectedRideID(ride._id); setTime(ride.date); setNumRiders(ride.numRiders)}}>
+                          <button
+                            onClick={() => {
+                              setIsOpen(true);
+                              setSelectedRideID(ride._id);
+                              // setTime(ride.date);
+                              setNumRiders(ride.numRiders);
+                            }}
+                          >
                             Update Ride
                           </button>
-                          {isOpen && <Modal setIsOpen={setIsOpen} rideid={selectedRideID} onSubmit={handleSubmit}/>}
+                          {isOpen && (
+                            <Modal
+                              setIsOpen={setIsOpen}
+                              rideid={selectedRideID}
+                              onSubmit={handleSubmit}
+                              setRideData={setRideData}
+                            />
+                          )}
                           {/* <button className="action-button" onClick={handleShowModal}>Update Ride</button> */}
-                          <button className="action-button" onClick={() => leaveRide(username, ride._id)}>Leave Ride</button>
+                          <button
+                            className="action-button"
+                            onClick={() => leaveRide(username, ride._id)}
+                          >
+                            Leave Ride
+                          </button>
                         </div>
                       </td>
                     </tr>
@@ -156,26 +202,32 @@ const RideHistory = ({username}) => {
 
         {rideData && (
           <div className="ride-history-table">
-          <h3>Past Rides</h3>
-          {pastRides.map((ride) => (
-            <table key={ride._id}>
-              <tbody>
-                <tr>
-                  <td><strong>Start:</strong></td>
-                  <td>{ride.locationFrom}</td>
-                </tr>
-                <tr>
-                  <td><strong>End:</strong></td>
-                  <td>{ride.locationTo}</td>
-                </tr>
-                <tr>
-                  <td><strong>Date:</strong></td>
-                  <td>{processDate(ride.date)}</td>
-                </tr>
-              </tbody>
-            </table>
-          ))}
-        </div>
+            <h3>Past Rides</h3>
+            {pastRides.map((ride) => (
+              <table key={ride._id}>
+                <tbody>
+                  <tr>
+                    <td>
+                      <strong>Start:</strong>
+                    </td>
+                    <td>{ride.locationFrom}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>End:</strong>
+                    </td>
+                    <td>{ride.locationTo}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>Date:</strong>
+                    </td>
+                    <td>{processDate(ride.date)}</td>
+                  </tr>
+                </tbody>
+              </table>
+            ))}
+          </div>
         )}
       </div>
     </div>
@@ -186,11 +238,13 @@ const App = ({ username, logout }) => {
   return (
     <div className="container">
       <div className="flex-container">
-        <Profile username={username}/>
-        <RideHistory username={username}/>
+        <Profile username={username} />
+        <RideHistory username={username} />
       </div>
       <div>
-        <button className="logout-button" onClick={() => logout(null)}>Logout</button>
+        <button className="logout-button" onClick={() => logout(null)}>
+          Logout
+        </button>
       </div>
     </div>
   );
