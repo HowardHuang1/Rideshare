76b11b1ca7aebf6233986efd593dc81edc769297
Aidan Robinson <120530116+arob26inson@users.noreply.github.com>
2023-06-04 14:41:05 -0700

    Create CreateRideModal.js

---
+++ src/components/CreateRideModal.js
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import "./Modal.css";
+import { RiCloseLine } from "react-icons/ri";
+import "font-awesome/css/font-awesome.min.css";
+import axios from "axios";
+import { set } from "mongoose";
+
+const username = localStorage.getItem("username");
+
+function CreateRideModal({ setIsOpen, rideid, onSubmit, setRideData }) {
+  console.log("rideid: " + setRideData);
+  const [pickupLocation, setPickupLocation] = useState();
+  const [destination, setDestination] = useState();
+  const [rideTime, setRideTime] = useState();
+  const [numRiders, setNumRiders] = useState();
+  const [AM, setAmPm] = useState("AM");
+  const rideId = rideid;
+
+  const putData = async (e) => {
+    await axios
+      .put("http://localhost:8000/update-ride", {
+        // default username
+        username: username,
+        rideID: rideId,
+        time: rideTime, // time is missing
+        AM: "false", // default am pm
+        numRidersAllowed: numRiders, // default numRiders
+      })
+      .then((res) => console.log("Posting data", res))
+      .catch((err) => console.log(err));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    console.log("handling input");
+
+    if (name === "rideTime") {
+      setRideTime(value);
+      console.log("rideTime: " + value);
+    } else if (name === "numRiders") {
+      setNumRiders(value);
+      console.log("numRiders: " + value);
+    }
+  };
+
+  const callSetState = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8000/get-rides-for-user",
+        { params: { username } }
+      );
+      setRideData(response.data);
+      console.log("SET RIDE DATA");
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // To prevent page reload on form submission
+    console.log("Click registered");
+    await putData();
+    await callSetState();
+    setIsOpen(false);
+    onSubmit();
+  };
+
+  return (
+    <div>
+      {/* <div className="darkBG centered" onClick={() => setIsOpen(false)}> */}
+      <div className="darkBG centered">
+        <div className="centered">
+          <div className="modal">
+            <div className="modalHeader">
+              <h5 className="title">Find Your Ride</h5>
+            </div>
+            <button className="closeButton" onClick={() => setIsOpen(false)}>
+              <RiCloseLine style={{ marginBottom: "-3px" }} />
+            </button>
+            <div className="modalContent">
+              <form onSubmit={handleSubmit}>
+                <div class="inputWithIcon2">
+                  <input
+                    type="text"
+                    name="rideTime"
+                    value={rideTime}
+                    placeholder="Time of Ride, ex: 8:30"
+                    onChange={handleInputChange}
+                  />
+                  <select
+                    style={{
+                      borderRadius: "20px",
+                      width: "20%",
+                      border: "2px solid #aaa",
+                      height: "80%",
+                    }}
+                    onChange={(e) => handleInputChange(e, "amPm")}
+                  >
+                    <option value="AM">AM</option>
+                    <option value="PM">PM</option>
+                  </select>
+                  <i class="fa fa-spinner fa-lg fa-fw" aria-hidden="true"></i>
+                </div>
+                <div class="inputWithIcon">
+                  <input
+                    type="text"
+                    name="numRiders"
+                    value={numRiders}
+                    placeholder="Number of Riders, ex: 5"
+                    onChange={handleInputChange}
+                  />
+                  <i class="fa fa-user fa-lg fa-fw" aria-hidden="true"></i>
+                </div>
+                <div className="modalActions">
+                  <div className="actionsContainer">
+                    <button className="CreateButton" type="submit">
+                      Create Ride
+                    </button>
+                    <button className="SearchButton" type="submit">
+                      Search
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateRideModal;
